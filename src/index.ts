79ef7dfa98cf8759e81e47f0149d3ce618c8c633
LukeWebster-Khan <Luke.Webster-Khan@theaccesspoint.co.uk>
2022-09-21 23:00:10 +0100

    created add note functionality with local storage

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
 import {v4 as uuidV4} from 'uuid';
 
-console.log(uuidV4());
+type Task = {
+    id: string,
+    title: string,
+    completed: boolean,
+    createdAt: Date,
+}
+const list = document.getElementById('list') as HTMLUListElement;
+const form = document.getElementById('new-task-form') as HTMLFormElement;
+const input = document.getElementById('new-task-title') as HTMLInputElement;
+
+
+form?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if(input?.value === "" || input?.value === null) return;
+    const newTask:Task = {
+        id: uuidV4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date()
+    }
+    tasks.push(newTask);
+
+    addNewListTime(newTask)
+    input.value = "";
+});
+
+const addNewListTime = (task: Task )=>{
+const item = document.createElement('li');
+const label = document.createElement('label');
+const checkbox = document.createElement('input');
+checkbox.addEventListener("change", ()=>{
+    task.completed = checkbox.checked;
+    saveTasks()
+   
+})
+checkbox.type = 'checkbox';
+checkbox.checked = task.completed;
+label.append(checkbox, task.title);
+item.append(label);
+list?.append(item)
+}
+
+const saveTasks = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+const loadTasks = () => {
+    const taskJson = localStorage.getItem('tasks')
+    if(taskJson === null) return []
+    return JSON.parse(taskJson)
+}
+const tasks: Task[] =  loadTasks()
+tasks.forEach(addNewListTime)
